dbe882b098406aaf55d12752fefaf354f31ec554
AndreRaffoul <andreraffoul@gmail.com>
2025-02-08 16:55:44 -0300

    Criando com Angular

---
+++ cliente/cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente',
+  moduleId: module.id,
+  templateUrl: 'cliente.component.html',
+  styleUrls: ['cliente.component.css']
+})
+export class ClienteComponent {
+  constructor() {
+  }
+}
